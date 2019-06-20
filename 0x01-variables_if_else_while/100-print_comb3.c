fd3b38d75f3466d05eb366ad8814bbb9cd532c1e
zinczar <brendan@wineisneat.com>
2019-06-20 23:35:59 +0000

    Betty for 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i+1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
@@ -24,5 +24,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-    return (0);
+return (0);
 }

40261636803c5b5230757533bf6a8f089a106dd7
zinczar <brendan@wineisneat.com>
2019-06-20 21:10:30 +0000

    added % to == 0 for #1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	if ((n % 10) > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, (n % 10));
-	else if (n == 0)
+	else if ((n % 10) == 0)
 		printf("Last digit of %i is %i and is 0\n", n, (n % 10));
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, (n % 10));

9df7abb735d7e5cd2e0c6ed07def0e08c3eeb747
zinczar <brendan@wineisneat.com>
2019-06-21 14:31:49 +0000

    progress on 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
-  
+
 /**
  * main - Entry point
- *                   
+ *
  * Return: Always 0 (Success)
- */                          
+ */
 int main(void)
-{             
-    return (0);
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = j + 1; l <= 9; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if ((i + j + k + l) < 197)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
 }

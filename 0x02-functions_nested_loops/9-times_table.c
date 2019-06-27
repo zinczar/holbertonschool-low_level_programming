f3241023f8c1a99b121c9206b38626adf7fc4086
zinczar <brendan@wineisneat.com>
2019-06-27 04:54:57 +0000

    0x03 setup

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (((i * j) / 10) == 0)
+			if ((j != 0) && ((i * j) / 10) == 0)
 			{
 				_putchar(' ');
 			}
@@ -22,6 +22,11 @@ void times_table(void)
 				_putchar(((i * j) / 10) + '0');
 			}
 			_putchar(((i * j) % 10) + '0');
+			if (j == 0)
+			{
+				_putchar(',');
+                                _putchar(' ');
+			}
 			if (j % 9 != 0)
 			{
 				_putchar(',');

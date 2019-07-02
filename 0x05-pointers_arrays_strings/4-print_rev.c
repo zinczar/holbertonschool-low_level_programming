acad84a05f0471589177c05839727932adb73a14
zinczar <brendan@wineisneat.com>
2019-07-02 17:28:57 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,40 @@
-#include holberton.h
+#include "holberton.h"
 
 /**
  * print_rev - code
+ * @s: Char
  *
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
-    return (0);
+	int i;
+
+	int strlen = _strlen(s);
+
+	for (i = strlen; s[i] >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+#include "holberton.h"
+
+/**
+ * _strlen - code
+ * @s: Char
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);

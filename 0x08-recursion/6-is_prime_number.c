518e10962f3d28ffd026c1e58487c9478ff0d222
zinczar <brendan@wineisneat.com>
2019-07-11 03:53:43 +0000

    EON

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,4 @@
-#include ”holberton.h”
+#include "holberton.h"
 
 /**
  * is_prime_number - code
@@ -8,6 +8,19 @@
  */
 int is_prime_number(int n)
 {
-Return (0);
+	int i = n / 2;
 
+	if (i == 1)
+	{
+		return (1);
+	}
+	else if (n % i  == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		i = i - 1;
+		is_prime_number(n);
+	}
 }

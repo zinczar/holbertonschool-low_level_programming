49114fff029d9b07e366dbefd4751ffcf029531b
zinczar <brendan@wineisneat.com>
2019-07-12 21:08:18 +0000

    Solved SQRT

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,4 @@
-#include ”holberton.h”
+#include "holberton.h"
 
 /**
  * _sqrt_recursion - code
@@ -8,6 +8,22 @@
  */
 int _sqrt_recursion(int n)
 {
-Return (0);
+	int i;
 
+	i = _sqrt_helper(0, 1, n);
+
+	return (i);
+
+}
+
+int _sqrt_helper(int counter, int path_to_sqrt, int n)
+{
+	if (n == 0)
+		return (counter);
+
+	else if (n < 0)
+		return (-1);
+
+	else
+		return (_sqrt_helper((counter + 1), (path_to_sqrt + 2), (n - path_to_sqrt)));
 }
--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -13,7 +13,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt_helper(int counter, int path_to_sqrt, int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 

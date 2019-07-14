479cf65fbac319bbb37f8064303b873856faf5db
zinczar <brendan@wineisneat.com>
2019-07-14 22:03:50 +0000

    changed incriment from 'i++' to 'i = i+2'

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i = i + 2)
 	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

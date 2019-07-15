ec098592f4d0a92ff1618a7778fe4894c639157d
zinczar <brendan@wineisneat.com>
2019-07-15 04:08:50 +0000

    First Shot at #4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
-/**                            
+/**
  * main - code
- *
+ * @argc: Int
+ * @argv: Char
+ *  *
  * Return: Always 0.
  */
-void main(void)
+int main(int argc, char *argv[])
 {
-Return (0);
+	int i, add;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) >= 0 && atoi(argv[i]) <= 9)
+		{
+			add = add + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", add);
+	return (0);
 }

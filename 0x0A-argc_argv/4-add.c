a2bc6fea55fd00de2228db533959cac8a5ddd569
zinczar <brendan@wineisneat.com>
2019-07-16 06:30:04 +0000

    added is_positive helper

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,16 +4,17 @@
  * main - code
  * @argc: Int
  * @argv: Char
- *  *
+ *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int i, add;
+	int i;
+	unsigned int add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) >= 0 && atoi(argv[i]) <= 9)
+		if (is_positive(argv[i]))
 		{
 			add = add + atoi(argv[i]);
 		}
@@ -23,6 +24,26 @@ int main(int argc, char *argv[])
 			return (1);
 		}
 	}
-	printf("%d\n", add);
+	printf("%u\n", add);
 	return (0);
+}
+
+#include "holberton.h"
+
+/**
+ * is_positive - code
+ * @s: Char
+ *
+ * Return: Always 0.
+ */
+int is_positive(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+			return (0);
+	}
+	return (1);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -3,10 +3,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int _putchar(char c);
 int main (int argc, char *argv[]);
 int main(int argc, __attribute__((unused))char *argv[]);
 int main(__attribute__((unused))int argc, char *argv[]);
+int is_positive(char *s);
 
 #endif

88047c0015d49410ec97a8625ae07b6c83d019c7
zinczar <brendan@wineisneat.com>
2019-07-17 21:31:10 +0000

    #5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -9,6 +9,38 @@
  */
 char *argstostr(int ac, char **av)
 {
-return (0);
+	int i, j, k = 0;
+	char *constring;
+	int strlen = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strlen++;
+		}
+		strlen++;
+	}
+
+	constring = malloc (sizeof(char) * (strlen +1));
+
+	if (constring == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+                for (j = 0; av[i][j] != '\0'; j++)
+                {
+                        constring[k] = av[i][j];
+			k++;
+		}
+                constring[k] = '\n';
+		k++;
+        }
+
+return (constring);
 
 }

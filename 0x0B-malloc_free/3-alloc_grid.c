3afb404122d6e17d34cd3d057b9a0e8038088290
zinczar <brendan@wineisneat.com>
2019-07-18 05:07:24 +0000

    seperated malloc for rows and columns

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,13 +15,25 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int) * (width * height));
+	grid = malloc(sizeof(int *) * height);
 
 		if (grid == NULL)
 			return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
+		grid[i] = malloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+		{
+			while (i > 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+		}
+
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;

62a00715b41179c185344f86c8464c13fdc3eb3c
zinczar <brendan@wineisneat.com>
2019-07-23 01:08:54 +0000

    added _putchar

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,6 +8,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

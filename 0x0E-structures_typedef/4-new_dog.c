4719cbd17d703c5b57d814db84689592f85d5e11
zinczar <brendan@wineisneat.com>
2019-07-23 02:46:56 +0000

    #4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * new_dog - Code
@@ -10,10 +12,60 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	dog_t *good_dog;
+	unsigned int name_len, owner_len;
 
-return (0);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
+	good_dog = malloc(sizeof(dog_t));
+	if (good_dog == NULL)
+		return (NULL);
+
+	good_dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (good_dog->name == NULL)
+		return (NULL);
+
+	good_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (good_dog->owner == NULL)
+		return (NULL);
+
+	_strcpy(good_dog->name, name);
+	good_dog->age = age;
+	_strcpy(good_dog->owner, owner);
+
+	return (good_dog);
+}
+
+/**
+ * _strlen - code
+ * @s: Char
+ *
+ * Return: Always 0.
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int strlen = 0, i = 0;
+
+	while (str[i] != '\0')
+	{
+		strlen++;
+		i++;
+	}
+	return(strlen);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	unsigned int src_len, i;
+
+	src_len = _strlen(src);
+
+	for (i = 0; i < src_len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }

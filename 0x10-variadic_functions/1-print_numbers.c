2d5955c4daa37104abd832f1dac972734ad2f478
zinczar <brendan@wineisneat.com>
2019-07-26 19:56:12 +0000

    #2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int arg = 0;
 	unsigned int i = 0;
 
-	if (separator == NULL)
-		separator = "";
-
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
 		arg = va_arg(valist, int);
 
-		if (i != 0)
+		if (i != 0 && separator != NULL)
 		{
 			printf("%s ", separator);
 		}
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,14 +2,34 @@
 
 /**
  * print_strings - Code
- * @seperator: Char
+ * @separator: Char
  * @n: Unsigned Int
  *
  * Return: 0
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list valist_strings;
+	char *arg;
+	unsigned int i = 0;
 
-return (0);
+	va_start(valist_strings, n);
 
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(valist_strings, char*);
+
+		if (i != 0 && separator != NULL)
+		{
+			printf("%s ", separator);
+		}
+
+		if (arg == NULL)
+			printf("(nil)");
+		else
+			printf("%s", arg);
+	}
+	printf("\n");
+
+	va_end(valist_strings);
 }

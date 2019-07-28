494bfe79f380cc6c280622cdf7d9ff87d5f110d1
zinczar <brendan@wineisneat.com>
2019-07-28 22:42:41 +0000

    1st draft of #3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,31 @@
  */
 void print_all(const char * const format, ...)
 {
+	va_list valist_all;
+	unsigned int i = 0;
 
-return (0);
+	va_start(valist_all, n);
+
+	while (format[i])
+	{
+		switch (format[i]);
+		{
+		case 'c':
+			printf("%c", arg[i]);
+			break;
+
+		case 'i':
+			printf("%i", arg);
+			break;
+
+		case 's':
+			printf("%s", type_str);
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(valist_all);
 
 }

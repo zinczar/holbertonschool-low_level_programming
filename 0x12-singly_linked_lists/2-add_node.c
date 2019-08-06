3ced844058d182779198276ea4bbec68050d0c7b
zinczar <brendan@wineisneat.com>
2019-08-06 03:03:54 +0000

    #2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,8 @@
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_note - code
+ * add_node - code
  * @head: list_t
  * @str: Char
  *
@@ -9,5 +10,17 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-return (0);
+	list_t *new_node;
+
+	new_node = malloc(sizeof(size_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
 }

baec7f0a5051c4eada1bce89b2adf707951397e4
zinczar <brendan@wineisneat.com>
2019-08-06 03:50:13 +0000

    #3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node_end - code
@@ -9,5 +10,50 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-return (0);
+	list_t *new_node = malloc(sizeof(size_t));
+	list_t *last;
+
+	last = *head;
+
+	if (!head || !str || !new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+
+	return (new_node);
+}
+
+/**
+ * _strlen - code
+ * @s: Char
+ *
+ * Return: Count
+ */
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (s[count])
+	{
+		count++;
+	}
+	return (count);
 }

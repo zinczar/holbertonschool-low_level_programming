8df070670bfd45a7a5a75c7aa986d704ca3a4a88
zinczar <brendan@wineisneat.com>
2019-08-06 19:22:49 +0000

    #2 & #3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,11 +5,18 @@
  * @head: listint_t
  * @n: Int
  *
- * Return: 0
+ * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-return (0);
+	if (!head || !new_node)
+		return (NULL);
 
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,6 +10,27 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
-return (0);
+	listint_t *new_node = malloc(sizeof(size_t));
+	listint_t *last;
 
+	if (!head || !new_node)
+		return (NULL);
+
+	last = *head;
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+
+	return (new_node);
 }

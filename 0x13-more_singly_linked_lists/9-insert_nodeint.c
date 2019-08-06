aad22d0300779dd201d5ac856a07cad7370414cb
zinczar <brendan@wineisneat.com>
2019-08-06 22:44:22 +0000

    #9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,11 +6,30 @@
  * @idx: unsigned int
  * @n: Int
  *
- * Return: 0
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int count = 0;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *previous;
 
-return (0);
+	if (!head || !new_node)
+		return (NULL);
 
+	new_node->n = n;
+
+	previous = *head;
+
+	while (previous)
+	{
+		if (count == (idx - 1))
+		{
+			new_node->next = previous->next;
+			previous->next = new_node;
+		}
+		previous = previous->next;
+		count++;
+	}
+	return (new_node);
 }

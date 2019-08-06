6c62821f38f4177ff2673830d55abf9f8328ad66
zinczar <brendan@wineisneat.com>
2019-08-06 03:19:28 +0000

    Betty for 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,7 +23,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
 	new_node->len = _strlen(str);

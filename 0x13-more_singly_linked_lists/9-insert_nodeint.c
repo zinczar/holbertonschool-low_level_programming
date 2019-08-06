67641e2162805fc2d810c951affa87c7135eabec
zinczar <brendan@wineisneat.com>
2019-08-06 23:38:18 +0000

    edge cases for #9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *previous;
 
-	if (!head || !new_node)
+	if (!*head || !head || !new_node)
 		return (NULL);
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
 
 	new_node->n = n;
 

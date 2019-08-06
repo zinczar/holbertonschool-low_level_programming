a3d805493f530b6cb1b635f72505d6cd76b77a23
zinczar <brendan@wineisneat.com>
2019-08-06 17:47:33 +0000

    #0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,7 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	unsigned int count = 0;
 
-return (0);
-
+	while (h)
+	{
+		printf("%i\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }

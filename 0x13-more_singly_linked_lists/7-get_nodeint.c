fca8f299703d1eba92d776233bf37abf19defecd
zinczar <brendan@wineisneat.com>
2019-08-06 20:54:57 +0000

    #7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,11 +5,22 @@
  * @head: listint_
  * @index: unsigned int
  *
- * Return: 0
+ * Return: the nth node of a listint_t linked list
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int count = 0;
+	listint_t *current = head;
 
-return (0);
+	if (!head)
+		return (NULL);
 
+	while (current)
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
+	return (NULL);
 }

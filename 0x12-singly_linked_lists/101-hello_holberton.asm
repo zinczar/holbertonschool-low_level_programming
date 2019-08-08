2bb1e7d74afcb06bda20507daf32e7b2e5faf11b
zinczar <brendan@wineisneat.com>
2019-08-08 02:28:04 +0000

    different method for assembly w/o int

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,21 @@
-        SECTION .data
-msg:	    db "Hello, Holberton", 0
-fmt:	    db "%s", 10, 0
+	extern printf
 
-	        SECTION .text
-	        extern printf
-	        global main
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:    db "%s", 10, 0
+
+	SECTION .text
+
+	global main
 main:
-	        mov esi, msg
-	        mov edi, fmt
-	        mov eax, 0
-	        call printf
+	push rbp
 
-	        mov ebx, 0
-	        mov eax, 1
-	        int 0x80
+	mov rdi, fmt
+	mov rsi, msg
+	mov rax, 0
+	call printf
+
+	pop rbp
+
+	mov rax, 0
+ret

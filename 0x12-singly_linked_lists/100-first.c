f395dd129ba188af3aeac130a413f0be16ef1f61
zinczar <brendan@wineisneat.com>
2019-08-08 01:31:16 +0000

    Betty for #100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
  * first - Code
  *
  * Return: 0
  */
 
-void first (void) __attribute__ ((constructor));
-
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

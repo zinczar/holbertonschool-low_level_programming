cff01c26e32d7d3c2ea5ddaf1b944ec4e4daace0
zinczar <brendan@wineisneat.com>
2019-08-08 01:02:52 +0000

    fixed Betty for 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,12 +26,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	for (i = 0; i < (idx-1); i++)
+	for (i = 0; i < (idx - 1); i++)
 	{
 		if (previous->next == NULL)
 			return (NULL);
 
-                previous = previous->next;
+		previous = previous->next;
 	}
 
 	new_node->next = previous->next;

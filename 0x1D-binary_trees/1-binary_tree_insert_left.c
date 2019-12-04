d6c0479b33cc16b2b21073c807e2bd95c45a39b5
zinczar <brendan@wineisneat.com>
2019-12-04 23:00:37 +0000

    improved comments

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * main - Code
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: binary_tree-T struct
+ * @value: Int
  *
- * Return: 0
+ * Return: Left_Node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {

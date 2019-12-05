eac76738377cfc9f9ea07f160f69f6f43708ff40
zinczar <brendan@wineisneat.com>
2019-12-05 22:46:12 +0000

    Betty fixes for 8 & 9

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -13,15 +13,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+	if (left > right)
+		return (left);
 	else
-	{
-		if (tree->left)
-			left = binary_tree_height(tree->left) + 1;
-		if (tree->right)
-			right = binary_tree_height(tree->right) + 1;
-		if (left > right)
-			return (left);
-		else
-			return (right);
-	}
+		return (right);
 }

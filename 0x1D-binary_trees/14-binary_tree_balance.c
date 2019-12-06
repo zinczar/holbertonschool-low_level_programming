096cd858eb014ae62ecccadbbe3f400e5abc3856
zinczar <brendan@wineisneat.com>
2019-12-06 19:03:12 +0000

    Betty fixes for 14, 15, 16, 17 & 18

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,30 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * main - Code
+ * binary_tree_height - a function that measures the height of a binary tree
+ *@tree: binary_tree_t
  *
- * Return: 0
+ * Return: Longest side
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        int left = 0;
-        int right = 0;
+	int left = 0;
+	int right = 0;
 
-        if (tree == NULL)
-                return (0);
-        else
-        {
-                if (tree->left)
-                        left = binary_tree_height(tree->left) + 1;
-                if (tree->right)
-                        right = binary_tree_height(tree->right) + 1;
-                if (left > right)
-                        return (left);
-                else
-                        return (right);
-        }
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
 
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ *@tree: pointer to the root node of the tree to measure the balance
+ * Return: 0 if NULL or the difference between left and right
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left = 0;
--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,15 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * main - Code
+ * binary_tree_uncle - find the uncle of a node
+ *@node: pointer to the node to find the uncle
  *
- * Return: 0
+ * Return: NULL if node is NULL or had no uncle, else return its uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL)
-		return NULL;
-	if (node->parent->n == node->parent->parent->right->n)
+	if (node == NULL || node->parent == NULL
+		|| node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
 	else
 		return (node->parent->parent->right);

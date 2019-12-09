716d181597fd7c831b703133303e1e0eb049343d
zinczar <brendan@wineisneat.com>
2019-12-09 01:33:24 +0000

    handled !text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (!text_content)
+	{
+		close(fd);
+		return(1);
+	}
+
 	content_len = _strlen(text_content);
 	written = write(fd, text_content, content_len);
 

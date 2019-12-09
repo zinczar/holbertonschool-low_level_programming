a9e67d7481312edc2b2ada5599aac2ca24b2f2c6
zinczar <brendan@wineisneat.com>
2019-12-09 01:25:27 +0000

    #1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,4 +10,39 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd, content_len;
+	ssize_t written = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	content_len = _strlen(text_content);
+	written = write(fd, text_content, content_len);
+
+	if (written == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * _strlen - code
+ * @s: Char
+ *
+ * Return: Count
+ */
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+
+	return (count);
 }

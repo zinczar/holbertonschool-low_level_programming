b1bb06aac65151d5e7a53b0e7c3794cea7e0145b
zinczar <brendan@wineisneat.com>
2019-12-09 01:00:21 +0000

    betty fixed 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 
 	if (!buffer)
-                return (0);
+		return (0);
 
 	read_count = read(fd, buffer, letters);
 

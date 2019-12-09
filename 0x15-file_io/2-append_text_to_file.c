801d76c2c42b43c9fded2abc6d16193722558bf9
zinczar <brendan@wineisneat.com>
2019-12-09 01:54:16 +0000

    #2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,4 +10,45 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, content_len;
+	ssize_t written = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	content_len = _strlen(text_content);
+	written = write(fd, text_content, content_len);
+
+	close(fd);
+
+	if (written == -1)
+		return (-1);
+
+	return (1);
+}
+
+/**
+ * _strlen - code
+ * @s: Char
+ *
+ * Return: Count
+ */
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+
+	return (count);
 }
